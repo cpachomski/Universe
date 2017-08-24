3aec065db6c64f6ddb35fbb86d92f134ed62df12
Christopher Pachomski <cpachomski@gmail.com>
2017-08-24 10:29:48 -0400

    updte dev client entry

--- webpack/entry/index.js
+++ webpack/entry/index.js
@@ -4,12 +4,12 @@ const devConfig = {
     app: [
         "babel-polyfill",
         "react-hot-loader/patch",
-        path.resolve(__dirname, "../../src/index")
+        path.resolve(__dirname, "../../src/client")
     ]
 };
 
 const prodConfig = {
-    index: path.resolve(__dirname, "../../src/index.js")
+    index: path.resolve(__dirname, "../../src/client.js")
 };
 
 export default (process.env.NODE_ENV === "production" ? prodConfig : devConfig);
